ff5601e2d468a0a93e120d997dcf38517800683c
saralouihrani <louihrani.sara@itiscannizzaro.edu.it>
2025-01-15 11:35:20 +0000

    aaa

--- Main.java
+++ Main.java
@@ -16,13 +16,22 @@ public class Main {
         int[][] matrix = new int[3][3];
         Random random = new Random();
         // COMPLETARE IL CODICE
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(100); // Numeri casuali tra 0 e 99
+            }
+        }
       
 
         // Stampa della matrice
         System.out.println("Matrice:");
         // COMPLETARE IL CODICE
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
         // Calcolo della somma degli elementi 
         int sum = calculateSum(matrix);
@@ -35,7 +44,11 @@ public class Main {
 
     public static int calculateSum(int[][] matrix) {
         int sum = 0;
-   
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+        }
 // COMPLETARE IL CODICE
       
         return sum;
@@ -43,7 +56,13 @@ public class Main {
 
     public static int findMax(int[][] matrix) {
         int max = matrix[0][0];
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
 // COMPLETARE IL CODICE
       
         return max;
